86067bcef4d056b1b66a0c4a4d591808de1ed15e
theSaneHatter <139697271+theSaneHatter@users.noreply.github.com>
2025-08-24 14:11:19 -0700

    this branch should only consist of code which will let the website be hosted. No untested, unstable code here mostly

--- lib.py
+++ lib.py
@@ -12,6 +12,9 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 import base64
+import nacl.utils
+import nacl.secret
+from nacl.public import PrivateKey, SealedBox, PublicKey
 
 def get_time():
     now = datetime.now()
@@ -19,13 +22,18 @@ def get_time():
     return time_string
 
 
-
-def log(message, logfile='./logs/logs.txt', ip='0.0.0.0'):
+def log(message, logfile='./logs/logs.txt', ip='0.0.0.0', encrypt=False, public_key=None):
     now = datetime.now()
     timestamp = now.strftime('%d/%m/%y') +','+ get_time()
-    with open(logfile, 'a') as file:
-        file.write(f'{timestamp}[{ip}]:>{message}<\n')
-    file.close()
+    log_msg = f'{timestamp}[{ip}]:>{message}<\n'
+    if encrypt:
+        if public_key==None:
+            print(f'\033[31m@error:log(): encryption wanted, but no public key given')
+        append_encrypted_message(public_key, logfile, log_msg)
+    else:
+        with open(logfile, 'a') as file:
+            file.write(f'{timestamp}[{ip}]:>{message}<\n')
+        file.close()
     return True
 
 def backup_logs(logfile='./logs/logs.txt', savefile='./logs/saves'):
@@ -70,134 +78,91 @@ def forword_to_all(foword_fn,path=os.path.join(os.getcwd())):
     data = load_json(path=path)
     #for i in data['nodes']:
 
-def encrypt(pub_key, data):
-    public_key = pub_key
-    if not type(data) == bytes:
-        og_data = data.encode('utf-8')
-    else:
-        og_data = data
-    encrypted = public_key.encrypt(
-        og_data,
-        padding.OAEP(
-            mgf = padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm = hashes.SHA256(),
-            label=None
-        )
-    )
+def fix_old_log_formating(path):
+    for file in os.listdir(path):
+        new_name = file.replace(':', '-')
+        os.rename(os.path.join(path, file), os.path.join(path, new_name))
+# encryption of logs :
+'''
+To gen asymmetric key pair etc:
+private_key = PrivateKey.generate()
+public_key = private_key.public_key
+box = SealedBox(public_key)
+'''
+
+def encrypt(public_key,data):
+    box = SealedBox(public_key)
+    if type(data) != bytes:
+        print(f'\033[33mWorning from encrypt(): data is not of type bytes. type:>{type(data)}<\033[0m')
+        data = bytes(data, 'utf-8')
+    encrypted = box.encrypt(data)
     return encrypted
 
-def encrypt_file(pub_key_path, file_path):
-    with open(pub_key_path, 'rb') as pub_key_file:
-        pub_key = pub_key_path.read()
-
-    with open(file_path, 'rb') as file:
-        og_data = og_file.read()
-    encrypted_data = encrypt(pub_key, og_data)
-    with open(file_path, 'wb') as file:
-        file.write(encrypted_data)
-    print(f'encrypted {file_path}')
-    return True
-
 def decrypt(private_key, data):
-    if not type(data) == bytes:
+    if type(data) != bytes:
+        print(f'\033[33mWorning from encrypt(): data is not of type bytes. type:>{type(data)}<')
         data = bytes(data, 'utf-8')
-    decrypted = private_key.decrypt(
-        data,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
+    box = SealedBox(private_key)
+    decrypted = box.decrypt(data)
     return decrypted
 
-def decrypt_file(private_key_path, data_path):
-    with open(priv_pem_path,'rb') as priv_pem_file:
-        private_key = serialization.load_pem_private_key(
-            priv_pem_file.read(),
-            password=None
-        )
-
-    with open(data_path, 'rb') as data_file:
-        data = data_file.read()
-
-    decrypted = decrypt(private_key, data)
-    print(f'decrypted file: {decrypted}')
-    return decrypted
-
-def load_priv_key(private_key_path):
-    with open(private_key_path, 'rb') as priv_pem_file:
-        private_key = serialization.load_pem_private_key(
-            priv_pem_file.read(),
-            password=None
-        )
+def load_private_key(private_key_path):
+    with open(private_key_path, 'rb') as file:
+        private_key = PrivateKey(file.read())
     return private_key
 
-def load_pub_key(public_key_path):
-    with open(public_key_path, 'rb') as pub_key_file:
-        public_key = serialization.load_pem_public_key(
-            pub_key_file.read()
-        )
+def load_public_key(public_key_path):
+    with open(public_key_path, 'rb') as file:
+        public_key = PublicKey(file.read())
     return public_key
 
-def gen_priv_key():
-    private_key = rsa.generate_private_key(
-        public_exponent = 65537,
-        key_size = 2048
-    )
-    print(f'genorated private key: {private_key}')
-    return private_key
-
-def gen_priv_pem(private_key):
-    prev_pem = private_key.private_bytes(
-        encoding = serialization.Encoding.PEM,
-        format = serialization.PrivateFormat.TraditionalOpenSSL,
-        encryption_algorithm = serialization.NoEncryption()
-    )
-    print(f'genorated private pem: {prev_pem}')
-    return prev_pem
-
-def gen_pub_key(private_key):
-    public_key = private_key.public_key()
-    return public_key
-
-def gen_pub_pem(public_key):
-    pub_pem = public_key.public_bytes(
-        encoding = serialization.Encoding.PEM,
-        format = serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    print(f'genorated public pem: {pub_pem}')
-    return pub_pem
-
-def decrypt_lines(private_key_path, file_path):
-    private_key = load_priv_key(private_key_path)
-    lines = []
-    with open(file_path, 'r+') as file:
-        for line in file:
-            print(f'encrypted line: {line}')
-            decrypted = decrypt(private_key, line)
-            lines.append(decrypted)
-            print(f'decrypted line: decrypted')
-    with open(file_path, 'w') as file:
-        for line in lines:
-            file.write(line)
+def save_key(file_path, key):
+   #if type(key) != bytes:
+   #    key = bytes(str(key),'utf-8')
+    with open(file_path, 'wb') as file:
+        file.write(key)
     return True
 
-import base64
-
-def encrypt_lines(public_key_path, file_path):
-    public_key = load_pub_key(public_key_path)
-    print(f'@debug:public key:{public_key}')
-    lines = []
-
-    # Read original lines
-    with open(file_path, 'r', encoding='utf-8') as file:
+def encrypt_file_line_by_line(public_key, file_path):
+    encrypted_lines = []
+    with open(file_path, 'rb') as file:
         for line in file:
-            encrypted = encrypt(public_key, line.strip())
-            encoded = base64.b64encode(encrypted).decode('utf-8')
-            lines.append(encoded)
+            encrypted_line = encrypt(public_key, line)
+            encrypted_lines.append(encrypted_line)
 
-    # Overwrite file with base64-encoded encrypted lines
-    with open(file_path, 'w', encoding='utf-8') as file:
-        for encoded_line in lines:
-            file.write(encoded_line + '\n')
+    with open(file_path, 'wb') as file:
+        for line in encrypted_lines:
+            line_len = len(line)
+            file.write(line_len.to_bytes(4, 'big'))  #write 4-byte length prefix
+            file.write(line)
+
+def decrypt_file_line_by_line(private_key, file_path):
+    decrypted_lines = []
+    with open(file_path, 'rb') as file:
+        while True:
+            len_bytes = file.read(4)  #read 4-byte length prefix
+            if not len_bytes:
+                break
+            line_len = int.from_bytes(len_bytes, 'big')
+            encrypted_line = file.read(line_len)
+            decrypted_line = decrypt(private_key, encrypted_line)
+            decrypted_lines.append(decrypted_line)
+
+    with open(file_path, 'wb') as file:
+        for line in decrypted_lines:
+            file.write(line)
+
+def append_message(file_path, message):
+    if type(message) != bytes:
+        message = bytes(message,'utf-8')
+
+    length = len(message)
+    with open(file_path, 'ab') as f:
+        f.write(length.to_bytes(4, 'big'))  # 4 bytes so 2**32 max size
+        f.write(message)
+
+def append_encrypted_message(public_key, file_path, message):
+    if type(message) != bytes:
+        message = bytes(message,'utf-8')
+    encrypted = encrypt(public_key, message)
+    append_message(file_path, encrypted)
